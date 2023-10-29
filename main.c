6deceb2091f0900d3722efb3e38df1a3ec23cd32
wierzba100 <filipwierzbinka@gmail.com>
2023-10-29 19:26:19 +0100

    Led On/Off webserver

--- main.c
+++ main.c
@@ -1,13 +1,32 @@
-#include <stdio.h>
-#include <stdlib.h>
-
+#include "lwip/apps/httpd.h"
 #include "pico/stdlib.h"
-#include "pico/stdio.h"
+#include "pico/cyw43_arch.h"
+#include "lwipopts.h"
+#include "cgi.h"
 
+const char WIFI_SSID[] = "Router_0";
+const char WIFI_PASSWORD[] = "tajemnica54321";
 
 int main() {
     stdio_init_all();
-    printf("Hello World\n");
+    cyw43_arch_init();
+    cyw43_arch_enable_sta_mode();
 
-    return 0;
+    // Connect to the WiFI network - loop until connected
+    while(cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PASSWORD, CYW43_AUTH_WPA2_AES_PSK, 30000) != 0)
+    {
+        printf("Attempting to connect...\n");
+    }
+    // Print a success message once connected
+    printf("Connected!\n");
+    
+    // Initialise web server
+    httpd_init();
+    printf("Http server initialised\n");
+
+    cgi_init();
+    printf("CGI Handler initialised\n");
+    
+    // Infinite loop
+    while(1);
 }
