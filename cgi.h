744eabaf6b86eb9ad2437c63b5243810319a2300
wierzba100 <filipwierzbinka@gmail.com>
2023-11-21 00:15:03 +0100

    motor control pins added

--- cgi.h
+++ cgi.h
@@ -4,6 +4,11 @@
 #include "lwip/apps/httpd.h"
 #include "pico/cyw43_arch.h"
 
+#define MOTOR_A_FORWARD 12
+#define MOTOR_A_BACKWARD 13
+#define MOTOR_B_FORWARD 14
+#define MOTOR_B_BACKWARD 15
+
 // CGI handler which is run when a request for /led.cgi is detected
 const char * cgi_led_handler(int iIndex, int iNumParams, char *pcParam[], char *pcValue[])
 {
@@ -23,6 +28,37 @@ const char * cgi_led_handler(int iIndex, int iNumParams, char *pcParam[], char *
     return "/index.html";
 }
 
+void set_motor_pins(bool motor_a_forw, bool motor_a_back, bool motor_b_forw, bool motor_b_back)
+{
+    gpio_put(MOTOR_A_FORWARD, motor_a_forw);
+    gpio_put(MOTOR_A_BACKWARD, motor_a_back);
+    gpio_put(MOTOR_B_FORWARD, motor_b_forw);
+    gpio_put(MOTOR_B_BACKWARD, motor_b_back);
+}
+
+const char * car_movement_handler(int iIndex, int iNumParams, char *pcParam[], char *pcValue[])
+{
+    if (strcmp(pcParam[0] , "direction") == 0)
+    {
+        if(strcmp(pcValue[0], "forward") == 0)
+        {
+            set_motor_pins(true,false,true,false);
+        }else if(strcmp(pcValue[0], "left") == 0)
+        {
+            set_motor_pins(false,false,true,false);
+        }else if(strcmp(pcValue[0], "right") == 0)
+        {
+            set_motor_pins(true,false,false,false);
+        }else if(strcmp(pcValue[0], "backward") == 0)
+        {
+            set_motor_pins(false,true,false,true);
+        }
+    }
+
+    // Send the index page back to the user
+    return "/index.html";
+}
+
 // tCGI Struct
 // Fill this with all of the CGI requests and their respective handlers
 static const tCGI cgi_handlers[] = {
@@ -30,11 +66,14 @@ static const tCGI cgi_handlers[] = {
         // Html request for "/led.cgi" triggers cgi_handler
         "/led.cgi", cgi_led_handler
     },
+    {
+        "/car_movement.cgi", car_movement_handler
+    },
 };
 
 void cgi_init(void)
 {
-    http_set_cgi_handlers(cgi_handlers, 1);
+    http_set_cgi_handlers(cgi_handlers, 2);
 }
 
 #endif //REMOTE_CONTROL_CAR_CGI_H
--- main.c
+++ main.c
@@ -3,14 +3,29 @@
 #include "pico/cyw43_arch.h"
 #include "lwipopts.h"
 #include "cgi.h"
+#include "hardware/gpio.h"
 
 const char WIFI_SSID[] = "Router_0";
 const char WIFI_PASSWORD[] = "tajemnica54321";
+
+void motor_pins_init(void)
+{
+    gpio_init(MOTOR_A_FORWARD);
+    gpio_init(MOTOR_A_BACKWARD);
+    gpio_init(MOTOR_B_FORWARD);
+    gpio_init(MOTOR_B_BACKWARD);
+    gpio_set_dir(MOTOR_A_FORWARD, GPIO_OUT);
+    gpio_set_dir(MOTOR_A_BACKWARD, GPIO_OUT);
+    gpio_set_dir(MOTOR_B_FORWARD, GPIO_OUT);
+    gpio_set_dir(MOTOR_B_BACKWARD, GPIO_OUT);
+}
 
 int main() {
     stdio_init_all();
     cyw43_arch_init();
     cyw43_arch_enable_sta_mode();
+
+    motor_pins_init();
 
     // Connect to the WiFI network - loop until connected
     while(cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PASSWORD, CYW43_AUTH_WPA2_AES_PSK, 30000) != 0)
